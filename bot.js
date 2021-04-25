1976032444dbf8c2a919a0bac9ab24aa8ff8e4a2
Trimpsuz <57452544+Trimpsuz@users.noreply.github.com>
2021-04-25 20:23:17 +0300

    Improve Code

--- bot.js
+++ bot.js
@@ -14,8 +14,6 @@ FINLAND FTW!
 */
 
 (()=>{
-    // Clear console
-
     console.clear()
 
     var event = new KeyboardEvent('keydown', {
@@ -23,40 +21,27 @@ FINLAND FTW!
         ctrlKey: true
     });
 
-    // Start interval
-
     document.dispatchEvent(event);
 
-    // Set total pop amount
-
-    var total = 0;
+    var totalpops = 0;
 
     var interval = setInterval(()=>{
         var vue = document.getElementById('app').__vue__;
 
-        // Has user been marked as bot?
-
         if(vue.bot){
-            console.log("%c You have been marked as a bot, clear your cookies.", "background: #a00; color: #fff");
+            console.log("%c You have been marked as a bot, clear your cookies.", "color: #a00");
             clearInterval(interval);
             return;
         }
 
-        // Ban prevention
-
         vue.sequential_max_pops = 0;
 
-        // Detect sendStats function run
 
         if(vue.accumulator == 0){
-            total += 800;
-            console.log(`[${new Date().toLocaleTimeString()}] %cAdded 800 pops, (Total: ${total})`, "color: #0f0");
-
-            // Add pops to top of screen counter
+            totalpops += 800;
+            console.log(`[${new Date().toLocaleTimeString()}] %cAdded 800 pops, (Total: ${totalpops})`, "color: #0f0");
 
             vue.counter += 800;
-
-			// Open and close cat's mount
 
             vue.open = true;
             setTimeout(()=>{
@@ -64,11 +49,9 @@ FINLAND FTW!
             }, 1000);
         }
 
-        // Add 800 pops
-
         vue.accumulator = 800;
         
     }, 1000);
 
-    console.log("%c Started bot. Waiting to send first request. ", "background: #050; color: #0f0");
+    console.log("%c Started bot. Waiting to send first request. ", "background: #00; color: #0f0");
 })();
